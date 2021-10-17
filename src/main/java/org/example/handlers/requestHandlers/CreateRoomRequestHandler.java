6f1923b55599adf43552b0b7c81be602af630b4f
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2021-10-17 10:08:11 +0530

    create room request if and only if the client is not the owner of another chat room fixed

--- src/main/java/org/example/handlers/requestHandlers/CreateRoomRequestHandler.java
+++ src/main/java/org/example/handlers/requestHandlers/CreateRoomRequestHandler.java
@@ -6,7 +6,9 @@ import org.example.models.client.IClient;
 import org.example.models.reply.ReplyObjects;
 import org.example.models.requests.AbstractRequest;
 import org.example.models.requests.CreateRoomRequest;
+import org.example.models.room.LocalRoom;
 import org.example.services.client.ChatClientServer;
+import org.example.services.client.UtilService;
 import org.json.simple.JSONObject;
 
 
@@ -14,7 +16,6 @@ public class CreateRoomRequestHandler extends AbstractRequestHandler {
     private CreateRoomRequest request;
     private boolean approved;
     private String roomId;
-    private String formerRoomId;
 
     public CreateRoomRequestHandler(AbstractRequest request, IClient client) {
         super((Client) client);
@@ -38,19 +39,26 @@ public class CreateRoomRequestHandler extends AbstractRequestHandler {
     }
 
     private boolean approveIdentity(String identity) {
-        if (validateIdentityValue(identity) && checkUniqueIdentity(identity)) {
+        if (validateIdentityValue(identity) && checkUniqueIdentity(identity) && checkIsARoomCreator()) {
             return checkUniqueIdentity(identity);
         } else return false;
     }
 
     private boolean validateIdentityValue(String identity) {
         if (identity.length() < 3 || identity.length() > 16) return false;
-        else return Character.isAlphabetic(identity.charAt(0));
+        else return UtilService.isAlphaNumeric(identity) ;
     }
 
     private boolean checkUniqueIdentity(String identity) {
         // todo:: Change to global room list
         return !ChatClientServer.localRoomIdLocalRoom.containsKey(identity);
+    }
+
+    private boolean checkIsARoomCreator(){
+        if (getClient().getRoom().equals(ChatClientServer.getMainHal())){
+            return true;
+        }
+        return !((LocalRoom) getClient().getRoom()).getOwner().equals(getClient().getIdentity());
     }
 
 }
