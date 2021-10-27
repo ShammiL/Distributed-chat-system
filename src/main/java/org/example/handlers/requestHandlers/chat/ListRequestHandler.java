2bee78124e4b5c8d8c257e871912b5cbf74e585d
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2021-10-27 15:59:24 +0530

    list

--- src/main/java/org/example/handlers/requestHandlers/chat/ListRequestHandler.java
+++ src/main/java/org/example/handlers/requestHandlers/chat/ListRequestHandler.java
@@ -1,9 +1,6 @@
 package org.example.handlers.requestHandlers.chat;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import io.netty.channel.ChannelId;
 import org.apache.log4j.Logger;
 import org.example.models.client.Client;
 import org.example.models.client.IClient;
@@ -12,21 +9,14 @@ import org.example.models.messages.chat.reply.ReplyObjects;
 import org.example.models.messages.chat.requests.chat.ListRequest;
 import org.example.models.messages.coordination.leader.reply.GlobalRoomResponse;
 import org.example.models.room.GlobalRoom;
-import org.example.models.room.Room;
 import org.example.models.server.LeaderState;
 import org.example.models.server.ServerState;
-import org.example.services.client.ChatClientServer;
 import org.example.services.coordination.MessageSender;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.net.ConnectException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class ListRequestHandler extends AbstractRequestHandler {
     private final ListRequest request;
@@ -49,11 +39,9 @@ public class ListRequestHandler extends AbstractRequestHandler {
             if (ServerState.getInstance().isCoordinator()) {
                 System.out.println("if coordiantor send list to client");
                 JSONObject response = ReplyObjects.roomList(getRooms());
-                System.out.println(response);
                 sendResponse(response);
             } else {
-                System.out.println("herrr");
-                sendRequestToLeader();
+                sendRequestToLeaderAndSendResponseTOClient();
 
             }
         } catch (InterruptedException | ConnectException e) {
@@ -62,34 +50,34 @@ public class ListRequestHandler extends AbstractRequestHandler {
         }
     }
 
-    private void sendRequestToLeader() throws InterruptedException, ConnectException {
+    private void sendRequestToLeaderAndSendResponseTOClient() throws InterruptedException, ConnectException {
         JSONObject response = null;
 
         response = MessageSender.requestRoomList(
                 ServerState.getInstance().getCoordinator()
         );
-        System.out.println(response.get("room_list"));
 
-//        Gson gson = new Gson();
-//        Map<String , GlobalRoom> rooms = gson.fromJson((JsonElement) response.get("room_list"), ConcurrentHashMap.class);
-//        for(Map.Entry<String, GlobalRoom> entry : rooms.entrySet()){
-//            GlobalRoom gRoom = entry.getValue();
-//            System.out.println(gRoom.getRoomId());
-//
-//        }
+        Gson gson = new Gson();
+        GlobalRoomResponse globalRoomResponse = gson.fromJson(response.get("room_list").toString(),
+                GlobalRoomResponse.class);
+        ArrayList<String> rooms = new ArrayList<>();
+        for (Map.Entry<String, GlobalRoom> entry : globalRoomResponse.getGlobalRoomList().entrySet()) {
+            rooms.add(entry.getKey());
+        }
 
+        JSONObject clientResponse = ReplyObjects.roomList(rooms);
+        sendResponse(clientResponse);
 
 
     }
 
     public ArrayList<String> getRooms() {
         ArrayList<String> rooms = new ArrayList<>();
-        for(Map.Entry<String, GlobalRoom> entry : LeaderState.getInstance().getGlobalRoomList().entrySet()){
+        for (Map.Entry<String, GlobalRoom> entry : LeaderState.getInstance().getGlobalRoomList().entrySet()) {
             GlobalRoom gRoom = entry.getValue();
             rooms.add((gRoom).getRoomId());
 
         }
-        System.out.println(rooms);
         return rooms;
     }
 }
